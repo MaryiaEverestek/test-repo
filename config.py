f89a6a1b8b83eee184759885925d733bcca5ee6e
MaryiaEverestek <134386605+MaryiaEverestek@users.noreply.github.com>
2023-05-29 13:27:50 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-bucket_name = os.getenv('BUCKET_NAME')
-expiration = os.getenv('EXPIRATION')
